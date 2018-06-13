45613d276a2e745c7dde5131a05536fb062c7fe4
AL2O3 <info@vguardcon.com>
2018-06-13 08:59:54 -0400

    Playing with PDB, PuDB, & other debuggers.

--- Chickens.py
+++ Chickens.py
@@ -1,3 +1,12 @@
 # Chickens for the farm.
 # Chickens are delicious
 # White & red chickens.
+# import pudb; pu.db
+# pudb3 chickens.py
+x = 14
+y = 12
+z = 100*7
+prod = x*y
+prod2 = x+y+z+f
+print(prod2)
+print(prod)
